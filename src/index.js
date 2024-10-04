463421e92d40fe11b5a1d5f30ebed82da519e347
Caslu-Parra <lucasparra2@outlook.com>
2024-10-04 11:09:40 -0300

    refact: altera as props do componente message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Roteiro from './componentes/left/roteiro'
 import Chat from './componentes/right/chat';
 import Message from './componentes/right/message';
 
-
 const root = createRoot(document.getElementById('main'));
 root.render(
     <div class="row g-0">
@@ -14,8 +13,8 @@ root.render(
             <Roteiro title='Fiona Green' img='https://bootdey.com/img/Content/avatar/avatar3.png' data='2023-10-01 15:35:37' />
         </Historico>
         <Chat>
-        <Message dtEnvio='2024-06-12 14:48:56' owner='you'>Hello, can you hear me?</Message>
-        <Message dtEnvio='2024-06-12 14:49:12' owner='trevor'>Hey human! Yes, I can</Message>
+            <Message dtEnvio='2024-06-12 14:48:56' owner='you'>Hello, can you hear me?</Message>
+            <Message dtEnvio='2024-06-12 14:49:12' owner='trevor'>Hey human! Yes, I can</Message>
         </Chat>
     </div>
 );
