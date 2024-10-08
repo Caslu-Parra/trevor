9ab0d9288286f3edf87c9a7a865ea6a3907a06ee
Mariana Gaspar <marianagaspar98@gmail.com>
2024-10-08 10:41:47 -0300

    feat: atualiza forms

---
+++ src/componentes/form/ComboBox.js
@@ -0,0 +1,21 @@
+export default function ComboBox(props) {
+    return (
+        <div id={props.id} className="form-group">
+            <label htmlFor={props.id} className="d-block">{props.label}</label>
+            <select
+                id={props.id}
+                value={props.value}
+                onChange={props.onChange}
+                required={props.required}
+                className="form-select" // Classe Bootstrap para estilização
+            >
+                <option value="">Selecione</option>
+                {props.options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ const root = createRoot(document.getElementById('main'));
 root.render(
     <div class="row g-0">
         <Historico>
-            <Roteiro title='Vanessa Tucker' img='https://bootdey.com/img/Content/avatar/avatar5.png' data='2024-06-12 07:10:56' />
-            <Roteiro title='William Harris' img='https://bootdey.com/img/Content/avatar/avatar2.png' data='2024-07-11 09:20:15' />
-            <Roteiro title='Fiona Green' img='https://bootdey.com/img/Content/avatar/avatar3.png' data='2023-10-01 15:35:37' />
+            <Roteiro title='Lisboa - 13 dias' img='https://bootdey.com/img/Content/avatar/avatar5.png' data='2024-06-12 07:10:56' />
+            <Roteiro title='Paris, Barcelona - 20 dias' img='https://bootdey.com/img/Content/avatar/avatar2.png' data='2024-07-11 09:20:15' />
+            <Roteiro title='Orlando, Miami, Tampa - 10 dias' img='https://bootdey.com/img/Content/avatar/avatar3.png' data='2023-10-01 15:35:37' />
         </Historico>
         <Chat>
-            <Message dtEnvio='2024-06-12 14:48:56' owner='you'>Hello, can you hear me?</Message>
-            <Message dtEnvio='2024-06-12 14:49:12' owner='trevor'>Hey human! Yes, I can</Message>
+        <Message dtEnvio='2024-06-12 14:49:12' owner='trevor'>Olá, eu sou o Trevor, seu assistente de viagem personalizado e vou te ajudar a ter um roteiro de viagem inesquecível. Preencha o formulário para que eu crie seu roteiro!</Message>
         </Chat>
         <Modal>
             <Forms/>
