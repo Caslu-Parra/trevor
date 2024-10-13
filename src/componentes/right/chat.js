9e30aec626ffcf45541cb2946dab92fee8bfe1e0
RenanDRN <renan.reis.negrao@gmail.com>
2024-10-13 15:16:39 -0300

    fix: arruma responsividade do botao

---
+++ src/componentes/right/chat.js
@@ -0,0 +1,79 @@
+import 'primeicons/primeicons.css';
+import { SpeedDial } from 'primereact/speeddial';
+import React, { useRef } from 'react';
+import { Toast } from 'primereact/toast';
+import trevorImg from '../../img/trevor-02.png';
+import './speedDial.css';
+
+export default function Chat(props) {
+    const toast = useRef(null);
+    const items = [
+        {
+            label: 'Salvar',
+            icon: 'pi pi-save custom-icon',
+            command: () => {
+                toast.current.show({ severity: 'info', summary: 'Salvar', detail: 'Roteiros salvos!' });
+            }
+        },
+        {
+            label: 'Refazer',
+            icon: 'pi pi-refresh custom-icon',
+            command: () => {
+                toast.current.show({ severity: 'success', summary: 'Refazer', detail: 'Novos roteiros sendo gerados!' });
+            }
+        }
+    ];
+
+    return (
+        <div className="col-12 col-sm-7 col-lg-8 col-xl-9">
+            <div className="py-2 px-4 border-bottom"> 
+                <div className="d-flex align-items-center">
+                    <div className="position-relative">
+                        <img src={trevorImg} className="rounded-circle mr-1" alt="Logomarca assistente Trevor" width="60" height="60" />
+                    </div>
+                    <div className="flex-grow-1 pl-3">
+                        <strong>Trevor</strong>
+                    </div>
+                </div>
+            </div>
+            <div>
+            </div>
+            {props.children}
+            <div style={{ position: 'relative', height: '150px' }}>
+                <Toast ref={toast} />
+                <div style={{
+                    position: 'absolute',
+                    right: '1rem',
+                    bottom: '1rem',
+                    width: '80px',
+                    height: '80px',
+                    backgroundColor: '#076df2',
+                    borderRadius: '50%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: -1
+                }}></div>
+                <SpeedDial 
+                    model={items} 
+                    direction="up" 
+                    style={{ 
+                        position: 'absolute', 
+                        right: '1rem', 
+                        bottom: '1rem',
+                        zIndex: 1
+                    }} 
+                    buttonStyle={{ 
+                        background: "#076df2", 
+                        color: "white", 
+                        border: "none", 
+                        width: '60px', 
+                        height: '60px', 
+                        borderRadius: '50%' 
+                    }} 
+                    className="custom-speed-dial"
+                />
+            </div>
+        </div>
+    );
+}
